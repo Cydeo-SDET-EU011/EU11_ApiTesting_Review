943c7de145ee4267587228b1cfe9260f6d29a3f4
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-09-20 19:59:36 +0300

    all tasks are solved.

--- src/test/java/offieHour/week1/Solutions.java
+++ src/test/java/offieHour/week1/Solutions.java
@@ -5,6 +5,8 @@ import io.restassured.http.*;
 import io.restassured.response.*;
 import org.junit.jupiter.api.*;
 
+import java.util.*;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -75,7 +77,84 @@ public class Solutions {
                 .and().headers("Transfer-Encoding",is("chunked"))
                 .and().body(containsString("Europe"))
                 .extract().response().headers().hasHeaderWithName("Date");
-        ;
+    }
+
+    /*
+    TASK 4 :
+- Given accept type is Json
+- Path param value- US
+- When users sends request to /countries/us
+- Then status code is 200
+- And Content - Type is Json
+- And country_id is US
+- And Country_name is United States of America
+ And Region_id is 2
+
+     */
+
+    @Test
+    public void task4(){
+        given().accept(ContentType.JSON)
+                .and().pathParam("value","US")
+                .when().get("/countries/{value}")
+                .then().statusCode(200)
+                .and().contentType("application/json")
+                .and().body("country_id",is("US"))
+                .body("country_name",is("United States of America"))
+                .body("region_id",is(2));
+    }
+
+    /*
+TASK 5 :
+ - Given accept type is Json
+ - Query param value - q={"department_id":80}
+ - When users sends request to /employees
+ - Then status code is 200
+ - And Content - Type is Json
+ - And all job_ids start with 'SA'
+ - And all department_ids are 80
+ - Count is 25
+     */
+
+    @Test
+    public void task5(){
+        given().accept(ContentType.JSON)
+                .and().queryParam("q","{\"department_id\":80}")
+                .when().get("/employees")
+                .then().statusCode(200)
+                .and().contentType("application/json")
+                .and().body("items.job_id",everyItem(startsWith("SA")))
+                .and().body("items.department_id",everyItem(is(80)))
+                .body("count",is(25));
+    }
+
+    /*
+     TASK 6 :
+ - Given accept type is Json
+ - Query param value q={“region_id":3}
+ - When users sends request to /countries
+ - Then status code is 200
+ - And all regions_id is 3
+ - And count is 6
+ - And hasMore is false
+ - And Country_name are;
+ Australia,China,India,Japan,Malaysia,Singapore
+     */
+
+    @Test
+    public void task6(){
+        given().accept(ContentType.JSON)
+                .and().queryParam("q","{\"region_id\":3}")
+                .when().get("/countries/")
+                .then().statusCode(200)
+                .and().contentType("application/json")
+                .body("items.region_id",everyItem(is(3)))
+                .body("count",is(6))
+                .body("hasMore",is(false))
+                .body("items.country_name",containsInRelativeOrder("Australia","China","India","Japan","Malaysia","Singapore"));
+//                .body("items.country_name",is(Arrays.asList("Australia","China","India","Japan","Malaysia","Singapore")));
+
+
 
     }
 }
